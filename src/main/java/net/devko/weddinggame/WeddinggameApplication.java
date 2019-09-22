ab0f0abae330d258478ec714c25c464119fb98e3
Tobias Kowalski <Tobias.Kowalski@itinera.de>
2019-09-22 12:58:50 +0200

    Initial commit

---
+++ src/main/java/net/devko/weddinggame/WeddinggameApplication.java
@@ -0,0 +1,13 @@
+package net.devko.weddinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddinggameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddinggameApplication.class, args);
+	}
+
+}
